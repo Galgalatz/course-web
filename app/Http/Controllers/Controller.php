4d5d9e34496c0abba6eb65ff7b5db37049951f6f
= <=>
2019-10-02 11:40:49 +0300

    collapse changes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,20 +6,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-<<<<<<< HEAD
+
 use Illuminate\Support\Facades\Auth;
-=======
->>>>>>> b1164325b7d11e19e4c0349af0cd50d85802827d
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-<<<<<<< HEAD
 
     public function user()
     {
         return Auth::user();
     }
-=======
->>>>>>> b1164325b7d11e19e4c0349af0cd50d85802827d
 }
